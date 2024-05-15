5cf82d70b4f2b880f3025b4803664bea8c5cdf08
Israel Rifarachi hoyos <irifarac@car10s3.42barcelona.com>
2024-05-15 10:48:28 +0200

    deleting old version

--- backupp6maigminirtMArc/src/geometricobj/obj/ft_check_sphere.d
+++
@@ -1,4 +0,0 @@
-obj/ft_check_sphere.o: ft_check_sphere.c geometricobj.h \
-  ../parser/parser.h ../maths/maths.h ../maths/../parser/parser.h \
-  ../cameras/cameras.h ../cameras/../parser/parser.h \
-  ../cameras/../maths/maths.h ../cameras/../window/window.h
